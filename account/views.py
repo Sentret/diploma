3f635f476092837091db5d313314bb3e388adf3a
Sentret <j.andrianov@gmail.com>
2018-03-18 18:08:03 +0300

    Личный кабинет

--- account/views.py
+++ account/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+from main.models import Event
+from main.models import EventSubscription
 
-def account(request):
-    return render(request, "account/account.html")
+def subscriptions(request):
+
+    subs = EventSubscription.objects.filter(subscriber=request.user)
+    return render(request, "account/account.html", {'subscriptions':subs})
+
+
+def created_event(request):
+	events = Event.objects.filter(creater=request.user)
+	return render(request, "account/events.html", {"events":events})
+
 
--- tripplanner/urls.py
+++ tripplanner/urls.py
@@ -14,7 +14,9 @@ urlpatterns = [
     path('eventpublish', main.views.EventPublish.as_view(), name='event_publish'),
     path('login', main.views.login, name='login'),
     path('eventinfo/<id>', main.views.event_page, name='event_page'),
-    path('account', account.views.account, name='account'),
+    path('account/subscriptions', account.views.subscriptions, name='account'),
+    path('account/events', account.views.created_event, name='events'),
+
     path('eventsuscribe', main.views.EventSubscriptionView.as_view(), name='event_subscribe'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
